f92ef8a855b03d6239a6ba04bfa08801aedf0dee
LAIBAMEHMOOD <laibamehmood033@gmaile.com>
2024-03-02 13:03:56 +0500

    Assigment No 6

---
+++ main.js
@@ -0,0 +1,4 @@
+var whitespaceName = "\n\t Arsalan Imran \t\n";
+console.log(whitespaceName);
+var Withoutwhitespace = whitespaceName.trim();
+console.log(Withoutwhitespace);
---
+++ main.ts
@@ -0,0 +1,5 @@
+let whitespaceName = "\n\t Arsalan Imran \t\n"
+console.log(whitespaceName);
+
+let Withoutwhitespace = whitespaceName.trim();
+console.log(Withoutwhitespace);
